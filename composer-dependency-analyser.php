9b32fe1a7ffa20f88d21cbcf52171574d8d5bbdd
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 15:27:24 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,10 +3,12 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
     ->addPathToScan(__DIR__ . '/config', isDev: false)
     ->addPathToScan(__DIR__ . '/src', isDev: false)
-    ->addPathToScan(__DIR__ . '/tests', isDev: true);
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    ->ignoreErrorsOnPackageAndPath('yiisoft/injector', __DIR__ . '/config', [ErrorType::DEV_DEPENDENCY_IN_PROD]);
